1fd2a097a78c738f565fd06e557c934548300fe7
jayfalls <j.falls@pm.me>
2023-09-17 18:57:11 +0200

    [FEATURE] Added saving and loading of data. Added the check and delete functions to the todo list items

--- script.js
+++ script.js
@@ -1,21 +1,69 @@
 // VARIABLES
 /// HTML Elements
 //// Todo Creator
-const todoDescriptorInput = document.getElementsByClassName("todo-descriptor-input")[0];
-const todoAddButton = document.getElementsByClassName("todo-add-button")[0];
+const todoDescriptorInput = document.querySelector(".todo-descriptor-input");
+const todoAddButton = document.querySelector(".todo-add-button");
 //// Todo List
-const todoList = document.getElementsByClassName("todo-list")[0];
+const todoList = document.querySelector(".todo-list");
 
 
-// TODO CREATION
+// INITIALISATION
+todoList.addEventListener("click", modifyTodoItem, false);
+loadData()
+
+
+// LOCAL DATA
+function saveData(){
+    localStorage.setItem("todo-items", todoList.innerHTML);
+}
+
+function loadData(){
+    todoList.innerHTML = localStorage.getItem("todo-items");
+}
+
+
+// TODO FUNCTIONS
+/// Creation
+//// Create new html list todo item
+function createTodo() {
+    const newTodo = document.createElement("li");
+    newTodo.innerHTML = todoDescriptorInput.value;
+    const deleteButton = document.createElement("span");
+    deleteButton.className = "delete-todo-button"
+    deleteButton.innerHTML = "\u00d7"; // \u00d7 is icon code for x
+    newTodo.appendChild(deleteButton)
+    return newTodo
+}
+//// Add new todo item to todo list
 function addTodo(){
+    todoList.appendChild(createTodo());
+    /// Clear the input field
+    todoDescriptorInput.value = "";
+    /// Save
+    saveData();
+}
+//// Check if todo text is valid
+function checkAddTodo(){
+    // Make sure input isn't empty
     if(todoDescriptorInput.value === ""){
         alert("You Must Write Something!");
         return;
     }
 
-    let newTodo = document.createElement("li");
-    newTodo.innerHTML = todoDescriptorInput.value;
-    todoList.appendChild(newTodo);
-    todoDescriptorInput.value = "";
+    addTodo();
+}
+
+/// Modifying
+function modifyTodoItem(event){
+    const clickedTarget = event.target
+    console.log(clickedTarget.tagName)
+    switch(clickedTarget.tagName){
+        case "LI":
+            clickedTarget.classList.toggle("todo-item-checked");
+            break;
+        case "SPAN":
+            clickedTarget.parentElement.remove();
+            break; 
+    }
+    saveData();
 }
